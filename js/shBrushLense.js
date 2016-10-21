300628fc015d97bfab4b118dfb776d5d7059ac57
SergioTaborda <sergiotaborda@yahoo.com.br>
2016-10-21 19:26:04 -0200

    - Changed type syntax to pos fixed - Added errasure section

--- js/shBrushLense.js
+++ js/shBrushLense.js
@@ -7,14 +7,14 @@
 
 	function Brush()
 	{
-		var keywords =	'abstract break switch case try catch finally default ' +
+		var keywords =	'abstract as break switch case try catch finally ' +
 						'continue default do while if else for ' +
-						'true false null ' +
-						'extends implements constructor factory ' +
-						'import export module ' +
+						'true false null none' +
+						'extends implements constructor ' +
+						'import export enhancement module ' +
 						'native new is ' +
 						'package module class object enum annotation interface trait ' +
-						'public private protected  ' +
+						'public private protected ' +
 						'in out val var selead override ' +
 						'return super synchronized this throw implicit ' 
 					//	+ 'static transient volatile strictfp ';
@@ -25,7 +25,7 @@
 			{ regex: /\/\*(?!\*\/)\*[\s\S]*?\*\//gm,					css: 'preprocessor' },	// documentation comments
 			{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,		css: 'string' },		// strings
 			{ regex: SyntaxHighlighter.regexLib.singleQuotedString,		css: 'string' },		// strings
-			{ regex: /\b([\d]+(\.[\d]+)?[NZSLGfdmi]?(E[\d]+)?|#([a-fA-F0-9]_)+|$([0-1]_)+)\b/gi,				css: 'value' },			// numbers
+			{ regex: /(\b([\d_]+(\.[\d]+)?[NZSLGfdmi]?(E[\d]+)?)\b)|#([a-fA-F0-9_])+|\$([0-1_])+/gi, css: 'value' },			// numbers
 			{ regex: /{{/g,												css: 'color1' },		// annotation @anno
 			{ regex: new RegExp(this.getKeywords(keywords), 'gm'),		css: 'keyword' }		// java keyword
 			];
